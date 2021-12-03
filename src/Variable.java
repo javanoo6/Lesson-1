12ac243155256abc082079dc2a23f2e8b48e6a40
javanoo6 <soboop@gmail.com>
2021-12-03 13:56:35 +0300

    переменные / данные о ноуте

---
+++ src/Variable.java
@@ -0,0 +1,32 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte monitors = 1;
+        short keyboard_letters = 32;
+        int RAM_memory = 8192;
+        long ethernet_speed_bytes_per_second = 262144000;
+        float screen_diagonal = 16.15f;
+        double weight = 2.7145d;
+        boolean system_starts = true;
+        char symbol = 'K';
+
+        System.out.println("специкация парамертов ноутбука-");
+        System.out.print("сколько мониторов-");
+        System.out.println(monitors);
+        System.out.print("сколько кнопок на клавиатуре-");
+        System.out.println(keyboard_letters);
+        System.out.print("сколько оперативной памяти-");
+        System.out.println(RAM_memory);
+        System.out.print("какая скорость интернета в бпс-");
+        System.out.println(ethernet_speed_bytes_per_second);
+        System.out.print("какая диагональ экрана-");
+        System.out.println(screen_diagonal);
+        System.out.print("сколько весит ноутбук-");
+        System.out.println(weight);
+        System.out.print("запускается ли ноутбук-");
+        System.out.println(system_starts);
+        System.out.print("все О-");
+        System.out.println(symbol);
+
+
+    }
+}
